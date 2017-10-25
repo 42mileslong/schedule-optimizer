9fe5b3153881b3e8d0b19b10b3bdf3c4ebf886bc
Ben Pankow <pankow.ben@gmail.com>
2017-10-25 18:51:15 -0500

    Add API endpoints

--- src/server/api/index.js
+++ src/server/api/index.js
@@ -6,17 +6,37 @@ var database = require('../database')
 // data pulled from the database.
 router.get('/', function(req, res) {
     database.Head.findOne({}, function(err, resp) {
+        res.send(resp.children);
+    });
+});
+
+router.get('/year/:id', function(req, res) {
+    database.Year.findById(req.params.id, function(err, resp) {
         res.send(resp);
     });
 });
 
-router.get('/year', function(req, res) {
-    database.Head.findOne({}, function(err, resp) {
-        database.Year.find({
-            '_id' : { $in : resp.children }
-        }, function(err2, resp2) {
-            res.send(resp2);
-        });
+router.get('/term/:id', function(req, res) {
+    database.Term.findById(req.params.id, function(err, resp) {
+        res.send(resp);
+    });
+});
+
+router.get('/subject/:id', function(req, res) {
+    database.Subject.findById(req.params.id, function(err, resp) {
+        res.send(resp);
+    });
+});
+
+router.get('/course/:id', function(req, res) {
+    database.Course.findById(req.params.id, function(err, resp) {
+        res.send(resp);
+    });
+});
+
+router.get('/section/:id', function(req, res) {
+    database.Section.findById(req.params.id, function(err, resp) {
+        res.send(resp);
     });
 });
 
